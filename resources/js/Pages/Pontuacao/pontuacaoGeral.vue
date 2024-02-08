c66b5ae884fe7b9037aac8b2b7f277c674b9718a
jooj finalboss <joaovitor02.2000@gmail.com>
2024-02-08 13:41:01 -0300

    Changes to host options

--- resources/js/Pages/Pontuacao/pontuacaoGeral.vue
+++ resources/js/Pages/Pontuacao/pontuacaoGeral.vue
@@ -314,7 +314,7 @@ const handleDates = async (newDates) => {
         endDate: dates.value[1]
     });
 
-    await form.post('fodase nao faz sentido'), {
+    await form.post('/pontuacao'), {
         onFinish: () => form.reset('startDate', 'endDate'),
     };
 };

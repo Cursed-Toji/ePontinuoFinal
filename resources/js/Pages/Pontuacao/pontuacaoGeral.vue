ba189d3f5c09fc46a4123fc12fa2636110f662a8
jooj finalboss <joaovitor02.2000@gmail.com>
2024-02-08 13:24:40 -0300

    only a test

--- resources/js/Pages/Pontuacao/pontuacaoGeral.vue
+++ resources/js/Pages/Pontuacao/pontuacaoGeral.vue
@@ -314,7 +314,7 @@ const handleDates = async (newDates) => {
         endDate: dates.value[1]
     });
 
-    await form.post(route('pontuacao.atualizar')), {
+    await form.post(route('teadskopskw')), {
         onFinish: () => form.reset('startDate', 'endDate'),
     };
 };
